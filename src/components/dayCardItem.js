bf115d38cc23d737e97027fa024de8d11ab1e40c
unknown <indianic@ind665.inic.local>
2018-07-16 19:28:22 +0530

    Add Image

--- src/components/dayCardItem.js
+++ src/components/dayCardItem.js
@@ -9,9 +9,16 @@ class DayCardItem extends Component {
     render() {
         return(
                 <div className="cards">
-                    <p>{this.props.day.weather[0].main}</p>
-                    <span><img src={this.props.day.weather[0].icon} alt={this.props.day.weather[0].icon} /></span>
-                    <p>{this.props.day.weather[0].description}</p>
+                    <div className="headerSection">
+                        <div className="titleHead">{this.props.day.weather[0].main}</div>
+                        <div className="imgHead">
+                            <img src={"image/" + this.props.day.weather[0].icon + ".png"} 
+                            height="80" alt={this.props.day.weather[0].icon} />
+                        </div>
+                        
+                    </div>
+                    
+                    <div className="description">{this.props.day.weather[0].description}</div>
                 </div>                 
         )
     }
